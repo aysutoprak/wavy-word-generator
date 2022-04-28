f603847e80906ec7545fc91e9850095c87980821
Aysu Toprak <moonwater99@hotmail.com>
2022-04-29 01:57:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Display from "./Display"
+import React, {useState} from 'react';
 
 function App() {
+  const [label, setLabel] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a word.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <input type="text" id="x" onChange={e => setLabel(e.target.value)}></input><br></br>
       </header>
+      <Display inputtxt={label}/>
     </div>
   );
 }
